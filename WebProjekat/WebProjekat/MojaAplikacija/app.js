7ca3fc1246dbbd44cf9cfa69c57280a13979f439
Ognjen Gatalo <ognjeng95@gmail.com>
2017-06-23 16:05:15 +0200

    Ubacila angular i bootstrap, napravila rutiranje za podforume i logovanje

---
+++ WebProjekat/WebProjekat/MojaAplikacija/app.js
@@ -0,0 +1,16 @@
+﻿var forum = angular.module('forum', ['ngRoute']); // inicijalizovana anglar aplikacja
+
+forum.config(function ($routeProvider) { // konfiguracija potrebnih stvari koje ce se koristiti u aplikaciji
+    $routeProvider.when('/', // kada se ucita 
+	{
+	    redirectTo: '/podforumi'
+
+	}).when('/podforumi',
+    {
+        templateUrl: 'MojaAplikacija/stranice/podforumi.html'
+    }).when('/logovanje',
+    {
+        templateUrl: 'MojaAplikacija/stranice/logovanje.html'
+    })
+
+});
