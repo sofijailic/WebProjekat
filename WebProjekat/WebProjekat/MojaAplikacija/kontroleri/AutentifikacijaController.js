4749b299c9490e55ba02ce76c38413f129596549
Sofija Ilic <sofijailic505@gmail.com>
2017-06-23 23:17:23 +0200

    Napravila logovanje, zastitila da ne moze da se dodje na stranicu za logovanje i registraciju ako je korisnik ulogovan

--- WebProjekat/WebProjekat/MojaAplikacija/kontroleri/AutentifikacijaController.js
+++ WebProjekat/WebProjekat/MojaAplikacija/kontroleri/AutentifikacijaController.js
@@ -1,5 +1,9 @@
-﻿forum.controller('AutentifikacijaController', function ($scope, autentifikacijaFabrika,$window) {
+﻿forum.controller('AutentifikacijaController', function ($scope, autentifikacijaFabrika,$window,$rootScope) {
     
+    if ($rootScope.ulogovan == true) { // ako je korisnik ulogovan, nemoj mu dozvoliti da ide na stranicu za registrovanje ili logovanje
+        $window.location.href = "#!/podforumi";
+    }
+
     $scope.registruj = function (korisnik) {
 
         if (korisnik.lozinka == korisnik.potvrdaLozinke) {
@@ -20,7 +24,20 @@
 
     $scope.uloguj = function (korisnik) {
         autentifikacijaFabrika.Uloguj(korisnik).then(function (odgovor) {
-            console.log(odgovor.data);
+            if (odgovor.data == null) {
+                alert('Ovaj korisnik nije registrovan');
+            }
+            else {
+                document.cookie = "korisnik=" + JSON.stringify({ korisnickoIme: odgovor.data.KorisnickoIme, uloga: odgovor.data.Uloga }) + ";expires=Thu, 01 Jan 2019 00:00:01 GMT;";
+                sessionStorage.setItem("korisnickoIme", odgovor.data.KorisnickoIme);
+                sessionStorage.setItem("uloga", odgovor.data.Uloga);
+                $rootScope.ulogovan = true;
+                $rootScope.korisnik = {
+                    korisnickoIme: odgovor.data.KorisnickoIme,
+                    uloga: odgovor.data.Uloga
+                };
+                $window.location.href = "#!/podforumi";
+            }
         });
     }
 
--- WebProjekat/WebProjekat/MojaAplikacija/kontroleri/GlavniController.js
+++ WebProjekat/WebProjekat/MojaAplikacija/kontroleri/GlavniController.js
@@ -1,3 +1,26 @@
-﻿forum.controller('GlavniController', function ($scope) {
+﻿forum.controller('GlavniController', function ($scope,$rootScope) {
+
+    if (document.cookie !== "") { // proverimo ako cookie nije prazan
+        console.log(document.cookie);
+        var cookieInfo = document.cookie.substring(9, document.cookie.length); // cookieInfo je sada sve ono sto se nalazi u cookiju znaci {korisnickoIme: "ime",uloga:"korisnik"}
+        var parsed = JSON.parse(cookieInfo); // parsiramo string u JSON objekat
+        sessionStorage.setItem("korisnickoIme", parsed.korisnickoIme); // stavljamo na sesiju korisnickoIme
+        sessionStorage.setItem("uloga", parsed.uloga); // i ulogu
+        $rootScope.ulogovan = true; // kazemo da je ulogovan
+        $rootScope.korisnik = { // stavljamo korisnika na rootScope
+            korisnickoIme: sessionStorage.getItem("korisnickoIme"),
+            uloga: sessionStorage.getItem("uloga"),
+        };
+    } else { // ako je cookie prazan stavljamo da korisnik nije ulogovan
+        $rootScope.ulogovan = false;
+    }
+
+    $scope.IzlogujSe = function () {
+        document.cookie = 'korisnik=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        sessionStorage.clear();
+        $rootScope.ulogovan = false;
+        $rootScope.korisnik = {};
+        document.location.href = "#!/logovanje";
+    }
 
 });
